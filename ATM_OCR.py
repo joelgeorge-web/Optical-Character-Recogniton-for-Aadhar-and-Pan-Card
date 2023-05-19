a9b2a1258ae7993016c67213605c4a1101bf7735
joelgerge-web <joelgeorge98765@gmail.com>
2023-05-19 18:25:30 +0530

    new files

---
+++ ATM_OCR.py
@@ -0,0 +1,26 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Load the image
+image_path = "atm_card.jpg"  # Replace with the actual image path
+image = cv2.imread(image_path)
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply Canny edge detection
+edges = cv2.Canny(gray, threshold1=30, threshold2=100)
+
+# Display the original image and edges
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(edges, cmap='gray')
+plt.title('Edges')
+plt.axis('off')
+
+plt.tight_layout()
+plt.show()
